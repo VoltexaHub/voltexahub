f2a7280e4d39d01b74c9d8bd53ddb2ddfe2e5a3d
joogiebear <joogiebear@users.noreply.github.com>
2026-03-11 11:39:29 -0500

    fix: add XSL stylesheet reference to sitemap output

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -32,6 +32,7 @@ class SitemapController extends Controller
             ->get();
 
         $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+        $xml .= '<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>' . "\n";
         $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
 
         // Home
